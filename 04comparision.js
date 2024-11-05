ddaf0685c5334a78d6bdaacb2062f2712aa7ee89
Arpan888 <98892585+Arpan888@users.noreply.github.com>
2024-11-05 20:12:41 +0000

    comparision of different datatypes

---
+++ 04comparision.js
@@ -0,0 +1,15 @@
+console.log("2">1);
+// js has automatically converted the string value into number 
+ 
+console.log(true > 4);
+
+console.log(null > 0);
+console.log(null ==0);
+console.log(null >=0);
+
+//===  it checks the value and the datatypes.
+
+console.log("4" === 4);
+
+
+
