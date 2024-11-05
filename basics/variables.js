57ebedea0925dbcecc2aa8d94525386d004cbc64
Arpan888 <98892585+Arpan888@users.noreply.github.com>
2024-11-05 02:51:31 +0000

    A basic study about variables in js

---
+++ basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553;
+let accountEmail = "chauarpa@kotak";
+var accountpass = "12345";
+
+console.log(accountId);
+
+
+accountCity = "Jaipur";
+
+console.table([accountEmail,accountId,accountpass,accountCity]);
