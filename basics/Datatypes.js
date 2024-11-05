5f7ba2159213a113a3f7da2d51306880568581da
Arpan888 <98892585+Arpan888@users.noreply.github.com>
2024-11-05 18:59:08 +0000

    Datatypes including typeof

---
+++ basics/Datatypes.js
@@ -0,0 +1,21 @@
+console.log("Arpan");
+
+// keep your code readable
+console.log(typeof "Arpan");
+
+/*
+primitive data types
+- number
+-bigint
+-string
+-boolean
+-symbol - to find uniqueness
+-null  -  it is a standalone val, represents empty value, it is an object
+-undefined - when the variable is declared but no value is given to it.
+typeof - is used to find the type of data type.
+*/
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+
