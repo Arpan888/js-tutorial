c5b5c40beac34c1e9f084743caeb975f3f9e7f20
Arpan888 <98892585+Arpan888@users.noreply.github.com>
2024-11-05 20:44:00 +0000

    datatypes summary important for interviw

---
+++ 05datatypeSummary.js
@@ -0,0 +1,27 @@
+const myFunc  = function(){
+    console.log("hello world");
+    
+
+}
+
+console.log(typeof myFunc);
+
+
+let myobj = {
+    name : "Arpan",
+    age : 27,
+};
+
+console.log(myobj);
+console.log(typeof myobj);
+
+let outTemp = null;
+console.log(typeof outTemp);
+
+let state;
+console.log(typeof state);
+
+let BigNum = 34567n;
+console.log(typeof BigNum);
+
+// it is important for interview purpose.
