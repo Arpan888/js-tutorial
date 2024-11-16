775248d0b85018d80b9ef26e38aedccfa1f36071
Arpan888 <98892585+Arpan888@users.noreply.github.com>
2024-11-16 10:10:56 +0000

    for loop with brk and continue

---
+++ loop/1loop.js
@@ -0,0 +1,21 @@
+//  for(let i =1;i<=10;i++){
+//     if(i === 5){
+//         console.log("5 is the best number");
+//     } 
+//     console.log(i);
+    
+//  }
+
+// print the tables from 1 to 10.
+
+// for(let i = 1;i<=10;i++){
+//     console.log(`This is the table of ${i}`);
+//     for(let j=1;j<=10;j++){
+//         console.log(`${i}*${j}=${i*j}`);
+        
+//     }
+    
+    
+// }
+
+//break and continue.......
